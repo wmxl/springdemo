6729ce525681f0f1ad51e52a270dc655fb897319
Tony <294036748@qq.com>
2022-10-02 21:34:19 +0800

    add @Slf4j

--- src/main/java/com/example/springdemo/TestController.java
+++ src/main/java/com/example/springdemo/TestController.java
@@ -1,14 +1,19 @@
 package com.example.springdemo;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Date;
+
 @RestController
+@Slf4j
 public class TestController {
 
 
     @GetMapping("/test")
     public String test() {
+        log.info("test {}", new Date());
         return "test controller ";
     }
 
